8295a5a7938adbbf89782e1fa12cffcd45b7c4e7
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-10-16 00:57:45 +0530

    ok

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,() => {
+    console.log("Server Listening on port : ",PORT)
+})
+
+app.get('/status',);
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const names = "shubham";
+console.log(names);
+
+var name1 = "prajapati";
+console.log(name1);
+
+let name2 = "shubh";
+console.log(name2);
+
